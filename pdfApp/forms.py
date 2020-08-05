448f2d2cd660d0f31c8a9e54f7e187298dc5a743
keyurparalkar <keur.plkar@gmail.com>
2020-08-05 09:01:51 +0530

    Added support for multiple file upload

--- pdfApp/forms.py
+++ pdfApp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class FileUploadform(forms.Form):
     # title = forms.CharField(max_length=50)
-    file = forms.FileField()
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
--- pdfApp/views.py
+++ pdfApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView
 from .forms import FileUploadform
 
 # Create your views here.
@@ -7,16 +8,32 @@ def file_upload(request):
     if request.method == "POST":
         form = FileUploadform(request.POST, request.FILES)
         if form.is_valid():
-            print(f"Form DATA = {request.FILES['file']}")
-            print(f"Form DATA-TYPE = {type(request.FILES['file'])}")
 
-            #trying to opn the file that we recieved in the form of memory:
-            form_data = request.FILES['file']
+            form_data = request.FILES.getlist('file')
+
             with open('op.pdf', 'wb+') as f:
-                for data in form_data.chunks():
-                    f.write(data)
+                for file in form_data:
+                    for data in file.chunks():
+                        f.write(data)
 
             return HttpResponseRedirect('')
     else:
         form = FileUploadform()
         return render(request, 'pdfApp/upload_view.html',{'form':form})
+
+#Class to handle multiple file uploads:
+# class file_upload(FormView):
+#     form_class = FileUploadform
+#     template_name = 'pdfApp/upload_view.html'
+#     success_url = ''
+
+#     def post(self, request, *args, **kwargs):
+#         form_class = self.get_form_class()
+#         form = self.get_form(form_class)
+#         files = request.FILES.getlist('file_field')
+#         if form.is_valid():
+#             for f in files:
+#                 print(f)
+#                 return self.form_valid(form)
+#         else:
+#             return self.form_invalid(form)
