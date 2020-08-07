b3a6623580ba1643197bca97c3915a8d5a9d4eb6
keyurparalkar <keur.plkar@gmail.com>
2020-08-07 09:00:40 +0530

    Download button POC

--- pdfApp/views.py
+++ pdfApp/views.py
@@ -16,13 +16,23 @@ def file_upload(request):
                     for data in file.chunks():
                         f.write(data)
                         data_chunks += data
-            data_chunks = HttpResponse(data_chunks, content_type='application/pdf')
-            data_chunks['Content-Disposition'] = 'filename=op_test.pdf'
+            # data_chunks = HttpResponse(data_chunks, content_type='application/pdf')
+            # data_chunks['Content-Disposition'] = 'filename=op_test.pdf'
+            
+            request.session['data_chunks'] = data_chunks
 
             return render(request, 'pdfApp/upload_view.html',{'form':form,'data_chunks': data_chunks})
+            # return data_chunks
     else:
         form = FileUploadform()
         if('data_chunks' in globals()):
             del globals()['data_chunks']
 
         return render(request, 'pdfApp/upload_view.html',{'form':form})
+
+def file_download(request):
+    data = request.session['data_chunks']
+    data = HttpResponse(data, content_type='application/pdf')
+    data['Content-Disposition'] = 'filename=op_test.pdf'
+    
+    return data
