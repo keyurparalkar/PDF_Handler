c3d9ea7b6a87c8b7a715d82d8dd55cdd9afa4645
keyurparalkar <keur.plkar@gmail.com>
2020-08-09 12:09:37 +0530

    Added UploadData Model to the database

--- pdfApp/views.py
+++ pdfApp/views.py
@@ -3,37 +3,43 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.edit import FormView
 from django.utils import timezone
 from .forms import FileUploadform
+from .models import UploadData
 import json
 import ast
-import time
 
 # Create your views here.
 def file_upload(request):
     if request.method == "POST":
         form = FileUploadform(request.POST, request.FILES)
         if form.is_valid():
-
             form_data = request.FILES.getlist('file')
-            data_chunks = b''
-            with open('op.pdf', 'wb+') as f:
-                for file in form_data:
-                    for data in file.chunks():
-                        data_chunks += data
-        
-            # print(f'Data = {base64.b64decode(data_chunks)}')
-            request.session['data_chunks'] = json.dumps(str(data_chunks))
 
-            return render(request, 'pdfApp/upload_view.html',{'form':form,'data_chunks': data_chunks})
-            # return data_chunks
+            for file_data in form_data:
+                instance = UploadData(upload=file_data)
+                instance.save()
+
+            return render(request, 'pdfApp/upload_view.html',{'form':form,'data_chunks': UploadData.objects.count()})
     else:
         form = FileUploadform()
         if('data_chunks' in globals()):
             del globals()['data_chunks']
+        
+        try:
+            if(UploadData.objects.count() > 0):
+                entries = UploadData.objects.all()
+                entries.delete()
+                entires.save()
+        except:
+            print('No PDF records found in database....')
 
         return render(request, 'pdfApp/upload_view.html',{'form':form})
 
 def file_download(request):
-    data = ast.literal_eval(json.loads(request.session['data_chunks']))
-    data = HttpResponse(data, content_type='application/pdf')
-    data['Content-Disposition'] = f'filename={timezone.now()}.pdf'
-    return data 
+    #Fetch the data from UploadData Table and display it on the console.
+    query = UploadData.objects.all()
+    print(query)
+
+    # data = ast.literal_eval(json.loads(request.session['data_chunks']))
+    # data = HttpResponse(data, content_type='application/pdf')
+    # data['Content-Disposition'] = f'filename={timezone.now()}.pdf'
+    return None
