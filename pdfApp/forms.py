636b8492c2892bb3de5d947951085cef7464328c
keyurparalkar <keur.plkar@gmail.com>
2020-08-16 13:08:38 +0530

    Added custom file field with file type check validators

--- pdfApp/forms.py
+++ pdfApp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
+from .custom_formfields import pdfFileUpload
 
 class FileUploadform(forms.Form):
-    # title = forms.CharField(max_length=50)
-    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'class': 'browse-ip'}))
+    # file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'class': 'browse-ip'}))
+    file = pdfFileUpload(widget=forms.ClearableFileInput(attrs={'multiple': True,'class': 'browse-ip'}))
