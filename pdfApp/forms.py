b33eca8135b4678828f20a11eab0018c7de43fdb
keyurparalkar <keur.plkar@gmail.com>
2020-08-19 09:01:55 +0530

    Customized File input button with label

--- pdfApp/forms.py
+++ pdfApp/forms.py
@@ -3,4 +3,6 @@ from .custom_formfields import pdfFileUpload
 
 class FileUploadform(forms.Form):
     # file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'class': 'browse-ip'}))
-    file = pdfFileUpload(widget=forms.ClearableFileInput(attrs={'multiple': True,'class': 'browse-ip'}))
+    file = pdfFileUpload(label="",widget=forms.ClearableFileInput(
+        attrs={'style':'display: none;','accept':'.pdf','multiple': True})
+        )
